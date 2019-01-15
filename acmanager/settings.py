c777fa3c1c3c4e1240ee24e620a4f26aa9a2d5fc
wzhxyz <wzh2012@outlook.com>
2019-01-15 09:42:11 +0800

    更换数据库配置为mysql，添加requirements.txt依赖项

--- acmanager/settings.py
+++ acmanager/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'acmanager.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'acmanager',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 #
